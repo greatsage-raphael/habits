5931aee75a8df221025ce05fbedb6e01a4b1b5c7
Lubwama Collins <bizzicole87@example.com>
2025-01-08 22:17:18 +0300

    Fix: footer fixed

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Footer() {
   return (
     <footer className="container mx-auto my-5 flex h-16 flex-col items-center justify-between space-y-3 border-t px-3 pt-4 text-center sm:h-20 sm:flex-row sm:pt-2 md:text-lg">
-      <div className='text-white'>
+      <div className='text-black'>
         Made with{' '}
         <a
           href=""
@@ -28,7 +28,7 @@ export default function Footer() {
           in kampala.
         </a>
       </div>
-      <div className="flex space-x-4 pb-4 sm:pb-0">
+      <div className="flex space-x-4 pb-4 sm:pb-0 text-black">
         <Link
           href="https://github.com/greatsage-raphael"
           className="group"
